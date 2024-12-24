34400e9f983aaccbd86dcb78b7b43d5ea16bb464
Anzal Sidhikh <anzalsidhikh@gmail.com>
2024-12-24 15:11:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://bookstore-frontend-roan.vercel.app/'],
+    origin: ['*'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Authorization', 'Content-Type'],
